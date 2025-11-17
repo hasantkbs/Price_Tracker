04466c5ab48299b67d60edb05211a949a14ccb81
HasanTekbas <hasanntekbas@hotmail.com>
2025-11-17 20:44:53 +0300

    update

--- backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import User
-from .product import Product, UserProduct
-from .price_history import PriceHistory
-from .notification_queue import NotificationQueue
--- backend/app/scrapers/scraper_factory.py
+++
@@ -1,8 +0,0 @@
-from .generic_scraper import GenericScraper
-# Import other specific scrapers here
-
-def get_scraper(url, scraper_class=GenericScraper): # Add scraper_class argument
-    # In the future, we can add logic here to select the correct scraper
-    # based on the domain of the url.
-    # For now, we only have the generic scraper.
-    return scraper_class(url) # Use scraper_class here
